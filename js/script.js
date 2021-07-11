6f88b7a94ab55033d80075e589843e758656b986
Kaung Myat Paing <kaungmyatpaing2110@gmail.com>
2021-07-12 00:09:35 +0630

    added scroll-top function

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $(document).ready(function () {
     $(".navbar").toggleClass("nav-toggle");
   });
 });
+
+$(window).on("scroll load", function () {
+  $("#menu").removeClass("fa-times");
+  $(".navbar").removeClass("nav-toggle");
+
+  if ($(window).scrollTop() > 0) {
+    $(".scroll-top").show();
+  } else {
+    $(".scroll-top").hide();
+  }
+});
