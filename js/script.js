3468c6fa760e52a8329ba5d6149e98acebb62239
Kaung Myat Paing <kaungmyatpaing2110@gmail.com>
2021-07-12 00:20:15 +0630

    added scroll-spy feature

--- js/script.js
+++ js/script.js
@@ -14,4 +14,18 @@ $(window).on("scroll load", function () {
   } else {
     $(".scroll-top").hide();
   }
+
+  //scroll-spy
+
+  $("section").each(function () {
+    let height = $(this).height();
+    let offset = $(this).offset().top - 200;
+    let top = $(window).scrollTop();
+    let id = $(this).attr("id");
+
+    if (top > offset && top < offset + height) {
+      $(".navbar ul li a").removeClass("active");
+      $(".navbar").find(`[href="#${id}"]`).addClass("active");
+    }
+  });
 });
